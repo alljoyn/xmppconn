6e68672b16b18dc9ac1013edb02ec118cb0c408b
akrokhin <akrokhin@affinegy.com>
2015-09-30 13:18:04 -0500

    Fixing scoping issues with try-catch block, and changing variable names per code review CC-319

--- src/common/xmppconnutil.h
+++ src/common/xmppconnutil.h
@@ -41,9 +41,9 @@ using std::endl;
 using std::istringstream;
 using std::ostringstream;
 
-const string ALLJOYN_GLOBAL_SUFFIX = "global.chariot";
+const string ALLJOYN_URL_SUFFIX = "global.chariot";
 const string ALLJOYN_XMPP_SUFFIX = "Chariot.Xmpp";
-const string ALLJOYN_XMPP_CONFIG_INTERFACE_NAME = "org.alljoyn.Config." + ALLJOYN_GLOBAL_SUFFIX;
+const string ALLJOYN_XMPP_CONFIG_INTERFACE_NAME = "org.alljoyn.Config." + ALLJOYN_XMPP_SUFFIX;
 const string ALLJOYN_XMPP_CONFIG_PATH = "/Config/Chariot/XMPP";
 
 namespace util {
